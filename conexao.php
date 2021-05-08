48246540f305218534a28ea52443c273cbd839fb
Almir Camolesi <camolesi@femanet.com.br>
2021-05-08 14:23:41 -0300

    Conexao em produção
    Versao intermediaria para salvar o Fiuza

--- conexao.php
+++ conexao.php
@@ -1,3 +1,14 @@
 <?php // Classe para conexão de banco de dados
+    class Conexao {
+            private static $dbNome = "rodeio"; 
+            private static $dbHost = "localhost";
+            private static $dbUser = "root"; 
+            private static $password = ""; 
 
+            private static $cont = null; 
+
+            public
+
+
+    }
 ?> 
