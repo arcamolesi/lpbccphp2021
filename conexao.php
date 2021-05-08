a3d9ffd3dd72f494ecb08a81547e3f711f45b75e
Almir Camolesi <camolesi@femanet.com.br>
2021-05-08 14:58:45 -0300

    Classe Conexao Banco Dados  Completa
    Classe completa da conexao com o banco de dados.

--- conexao.php
+++ conexao.php
@@ -5,10 +5,28 @@
             private static $dbUser = "root"; 
             private static $password = ""; 
 
-            private static $cont = null; 
+            private static $con = null; 
 
-            public
+            public function __construct()
+            {
+                die("A Função init não é permitida...");
+            }
 
+            public static function conectar(){
+                if (self::$con == null){
+                    try {
+                        self::$con =  new PDO("mysql:host=" . self::$dbHost . 
+                                               ";dbname=". self::$dbNome, self::$dbUser, self::$password); 
+                    } catch (PDOException $exception) {
+                        die("Erro conexao " . $exception->getMessage()); 
+                    }
+                }
+                return self::$con; 
+            }
+
+            public static function desconectar(){
+                self::$con = null; 
+            }
 
     }
 ?> 
