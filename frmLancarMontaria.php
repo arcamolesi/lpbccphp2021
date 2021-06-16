00ef34561178f34e03d26182d5ab2d868de2ccb7
Almir Camolesi <camolesi@femanet.com.br>
2021-06-15 23:00:59 -0300

    Atualização com movimentação
    Movimentação com atualização de tabelas, select option e javascript para calcular soma das notas

--- frmLancarMontaria.php
+++ frmLancarMontaria.php
@@ -1,16 +1,34 @@
+<?php
+    include 'conexao.php'; 
+    $pdo = Conexao::conectar(); 
+  
+    $sql = "Select * from competidor order by nome "; 
+    $listaCompetidores = $pdo->query($sql); 
 
+    $sql = "Select * from animal order by nome "; 
+    $listaAnimais = $pdo->query($sql); 
+
+    Conexao::desconectar(); 
+?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+      <!-- Compiled and minified CSS -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    
+
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+   
+    <script type = "text/javascript"
+         src = "https://code.jquery.com/jquery-2.1.1.min.js"></script>           
+      <script src = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js">
+      
+      </script> 
     
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -24,31 +42,73 @@
         <div class="row">
             <form action="lancarMontaria.php" method="post"  id="frmLancarMontaria" class="col s12">
                 <div class="input-field col s8">
-					<label for="lblData">Informe a Data da Montaria</label>
                     <input type="date" class="form-control" id="txtData" name="txtData">
-                   
+					<label for="lblData">Informe a Data da Montaria</label>
+                         
                 </div>   
                         
+            <!--       
                 <div class="input-field col s8">
                     <label for="lblCompetidor">Informe o Competidor</label>
                     <input type="text" class="form-control" id="txtCompetidor" name="txtCompetidor">
                 </div>   
-
+            
                 <div class="input-field col s8">
                     <label for="lblAnimal">Informe o Animal</label>
                     <input type="text" class="form-control" id="txtAnimal" name="txtAnimal">
-                </div>   
-                 
+                </div>  
+            -->
+
+                <div class="input-field col s12">
+                <select id="txtCompetidor" name="txtCompetidor" class="<form-control">
+                  <?php  // carregar lista no select option
+					$i=0;
+                    foreach ($listaCompetidores as $competidor){
+                       if ($i==0) {?>
+                        <option value="" disabled selected>Escolha um Competidor</option>
+					     
+					   <?php } else { ?>
+                          <option value="<?php echo $competidor['id']; ?>"><?php echo $competidor['nome']; ?></option>
+				    <?php
+					   }
+				     $i++;
+                  }
+                  ?>
+                </select>
+                </div>
+
+                <div class="input-field col s12">
+                <select id="txtAnimal" name="txtAnimal" class="<form-control">
+                  <?php  // carregar lista no select option
+					$i=0;
+                    foreach ($listaAnimais as $animal){
+                       if ($i==0) {?>
+                        <option value="" disabled selected>Escolha um Animal</option>
+					     
+					   <?php } else { ?>
+                          <option value="<?php echo $animal['id']; ?>"><?php echo $animal['nome']; ?></option>
+				    <?php
+					   }
+				     $i++;
+                  }
+                  ?>
+                </select>
+                </div>
+
                 <div class="input-field col s5">
                     <label for="lblNtComp">Informe a Nota do Competidor</label>
-                    <input type="number" class="form-control" id="txtNtComp" name="txtNtComp">
+                    <input type="number" class="form-control" id="txtNtComp" name="txtNtComp"  onblur="calcular()">
                 </div>  
 
                 <div class="input-field col s5">
                     <label for="lblNtAnm">Informe a Nota do Animal</label>
-                    <input type="number" class="form-control" id="txtNtAnm" name="txtNtAnm">
+                    <input type="number" class="form-control" id="txtNtAnm" name="txtNtAnm" onblur="calcular()">
                 </div>  
 
+                <div class="input-field col s12">
+                        <label for="lblNotaFinal"><b>Nota Final: </b>  <label id="total"></label> </label>
+                </div>
+                
                 <div class="input-field col s8">
                     <br>
                     <button class="btn waves-effect waves-light" type="submit" name="btnGravar">Gravar
@@ -68,3 +128,26 @@
     </div>
 </body>
 </html>
+
+
+
+<script>
+function calcular(){
+
+    var valor1 = parseInt(document.getElementById('txtNtComp').value, 10);
+    var valor2 = parseInt(document.getElementById('txtNtAnm').value, 10);
+    var nota = valor1+valor2; 
+    if (isNaN(nota)) 
+        nota = 0;
+  
+    document.getElementById("total").innerHTML = nota.toFixed(2); 
+
+    //document.getElementById('result').value = result;
+}
+</script>
+
+<script>
+         $(document).ready(function() {
+            $('select').material_select();
+         });
+ </script>
--- lancarMontaria.php
+++ lancarMontaria.php
@@ -2,23 +2,44 @@
     include 'conexao.php'; 
   
     $data = trim($_POST['txtData']); 
-    $competidor = trim ($_POST['txtCompetidor']); 
-    $animal = trim($_POST['txtAnimal']);
+    $competidorID = trim ($_POST['txtCompetidor']); 
+    $animalID = trim($_POST['txtAnimal']);
     $ntCompetidor = trim($_POST['txtNtComp']); 
     $ntAnimal = trim($_POST['txtNtAnm']);
-   // $nota = $ntCompetidor + $ntAnimal; 
+    $nota = floatval($ntCompetidor) + floatval($ntAnimal); 
 
-    if (!empty($competidor) && !empty($animal) && !empty($nota)){
+    if (!empty($competidorID) && !empty($animalID) && !empty($nota)){
        $pdo = Conexao::conectar(); 
        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
        //lancamento de montaria na tabela montaria
        $sql = "INSERT INTO montaria (data, animal, competidor, notacompetidor, notaanimal) VALUES (?, ?, ?, ?, ?);"; 
        $query = $pdo->prepare($sql); 
-       $query->execute(array($data, $animal, $competidor, $ntAnimal, $ntCompetidor)); 
+       $query->execute(array($data, $animalID, $competidorID, $ntAnimal, $ntCompetidor)); 
 
        //atualização da tabela Competidor
+       $sql = "SELECT * FROM competidor WHERE id=?;";
+       $query = $pdo->prepare($sql); 
+       $query->execute(array($competidorID));
+       $competidor = $query->fetch(PDO::FETCH_ASSOC);
 
+      
+       $novaNota = $competidor['somanota'] + $nota; 
+
+       $sql = "UPDATE competidor SET somanota=? WHERE id=?"; 
+       $query = $pdo->prepare($sql); 
+       $query->execute (array($novaNota, $competidorID)); 
+
+       /* simulando uma atualização de estoque do tipo venda
+       if ($nota <= $competidor['somanota']){
+
+         $novaNota = $competidor['somanota'] - $nota; 
+
+         $sql = "UPDATE competidor SET somanota=? WHERE id=?"; 
+         $query = $pdo->prepare($sql); 
+         $query->execute (array($novaNota, $competidorID)); 
+  
+       } */ 
 
        Conexao::desconectar(); 
     }
